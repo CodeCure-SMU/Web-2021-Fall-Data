79fa8fce7daedb479fc80ce22e8c663b69d3304e
wrathlion <taejin7824@gmail.com>
2021-11-02 02:09:42 +0900

    Add Week 4 Codes

---
+++ Week4/variable_basic.js
@@ -0,0 +1,39 @@
+// variable_basic.js;
+const var1 = 1;
+let var2 = 1;
+
+console.log(var1, var2);
+
+// const는 값 변경 불가!
+// var1 = 2;
+
+var2 = 2;
+console.log(var1, var2);
+
+var2 = "Hello, World!";
+console.log(var1, var2);
+
+const integer = 10;
+const float1 = 3.141592;
+const boolean1 = true;
+const string1 = "Hello";
+
+console.log(integer + integer);
+console.log(7 - 4);
+console.log(5 * float1);
+console.log(3 / integer);
+console.log(10 % 3);
+
+console.log(string1 + integer);
+console.log(string1 + boolean1);
+
+const input = prompt("Input some words!");
+document.write("<br />" + input);
+
+const a = prompt("Input a");
+const b = prompt("Input b");
+console.log(a + b);
+
+const c = parseInt(prompt("Input c"));
+const d = parseInt(prompt("Input d"));
+console.log(c + d);
