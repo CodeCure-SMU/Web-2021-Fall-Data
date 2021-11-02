2c00d3849dc2fc21cd066e729eb73bd19dad9a07
Jin-MacBook <taejin7824@gmail.com>
2021-11-02 13:19:00 +0900

    Add Week 4 Practice Codes

---
+++ Week4/external_reference.js
@@ -0,0 +1,2 @@
+// external_reference.js;
+console.log("External reference");
---
+++ Week4/variable_intermediate.js
@@ -0,0 +1,9 @@
+// variable_intermediate.js
+const array = [10, 20, 30, 40, 50];
+
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[4]);
+console.log(array[5]);
